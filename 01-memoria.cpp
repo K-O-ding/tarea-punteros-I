40b2c01a99dab89cbf0a81e8bf49daf4e044c64b
Testname2 <134820349+K-O-ding@users.noreply.github.com>
2025-09-28 16:42:37 -0500

    Se termina 01-memoria.cpp y se añade respuestas a punterosI.md

--- 01-memoria.cpp
+++ 01-memoria.cpp
@@ -4,9 +4,18 @@ using namespace std;
 int main(){
 
     // Declaramos las variables
+    char c = 'a';
     int i = 5;
     float f = 3.14; 
-    char c = 'a';
 
+    // Imprimimos los valores
+    cout<<endl<<i<<endl;
+    cout<<f<<endl;
+    cout<<c<<endl;
+
+    // Imprimimos las direcciones de memoria
+    cout<<endl<<&i<<endl;
+    cout<<&f<<endl;
+    cout<<&c<<endl;
     return 0;
 }
