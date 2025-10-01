82b063a6babdd6cf92ec2e794eac80e861c6e77c
Testname2 <134820349+K-O-ding@users.noreply.github.com>
2025-10-01 17:40:48 -0500

    Se termina 12.4-buscar-subcadena-con-punteros.cpp

--- 12.4-buscar-subcadena-con-punteros.cpp
+++ 12.4-buscar-subcadena-con-punteros.cpp
@@ -28,7 +28,35 @@ int main(){
 }
 
 int buscarSubcadena(const char* str, const char* sub_str){
-    
+    // Se declara un entero que almacena el tamaño de la subcadena
+    int tam_sub = strlen(sub_str);
+    // Se declara un contador (cuenta cuantas letras coinciden en ambas cadenas)
+    int k = 0;
+
+    // Se recorre la cadena principal desde el inicio hasta la posicion final - tam_sub + 1
+    for (int i = 0; i < strlen(str)- tam_sub + 1; i++)
+    {
+        // Se recorre cada elemento de la cadena principal hasta 'tam_sub' veces
+        for (int j = 0; j < tam_sub; j++)
+        {
+            // Se compara cada elemento de ambas cadenas hasta 'tam_sub' veces
+            if (*(str + i + j) == *(sub_str + j))
+            {
+                // Si los caracteres coinciden, se incrementa el contador
+                k++;
+            }
+            if (k == tam_sub)
+            {
+                // Si el contador es igual al tamaño de la subcadena, retorna la posicion del inicio de la subcadena (i)
+                return i;
+            }
+        }
+        // Si el contador no llega a ser igual a 'tam_sub', se reinicia su valor a 0
+        k=0;
+    }
+
+    // Si no se encontro la subcadena, retorna -1
+    return -1;
 }
 
 void imprimirPosicion(const char* sub_str, int pos){
@@ -37,6 +65,6 @@ void imprimirPosicion(const char* sub_str, int pos){
         cout<<"\nLa subcadena '"<<sub_str<<"' se encontro en la posicion "<<pos<<endl;
     }else
     {
-        cout<<"\nNo se encontro la subcadena '"<<sub_str<<"'";
+        cout<<"\nNo se encontro la subcadena '"<<sub_str<<"'"<<endl;
     }
 }
