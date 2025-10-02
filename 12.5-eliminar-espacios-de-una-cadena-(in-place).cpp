49139dfc5c14d1acd2ea52f72568bad600954eb8
Testname2 <134820349+K-O-ding@users.noreply.github.com>
2025-10-01 20:17:54 -0500

    Se añade comentarios en 12.5-eliminar-espacios-de-una-cadena-(in-place).cpp

--- 12.5-eliminar-espacios-de-una-cadena-(in-place).cpp
+++ 12.5-eliminar-espacios-de-una-cadena-(in-place).cpp
@@ -6,28 +6,38 @@ using namespace std;
 void eliminarEspacios(char* str);
 
 int main(){
+    // Se declara una cadena con espacios
     char str[] = "e j e r c i c i o s";
 
+    // Se llama a la funcion para eliminar los espacios de la cadena
     eliminarEspacios(str);
 
+    // Se imprime la cadena sin espacios
     cout<<"\nLa cadena sin espacios es: "<<str<<endl;
+
     return 0;
 }
 
 void eliminarEspacios(char* str){
+    // Se declara una variable temporal para realizar el intercambio
     char temp;
 
     for (int i = 0; i < strlen(str); i++)
     {
+        // Se recorre la cadena hasta que se encuentra un espacio
         if (*(str + i) == ' ')
         {
             for (int j = i; j < strlen(str); j++)
             {
+                // Con otro indice, se recorre la cadena desde el espacio hasta un caracter distinto del espacio
                 if (*(str + j) != ' ')
                 {
+                    // Si se encuentra un caracter distinto del espacio se intercambia con el espacio
                     temp = *(str + i);
                     *(str + i) = *(str + j);
                     *(str + j) = temp;
+
+                    // Se aumenta el indice con el que se encontro el espacio
                     i++;
                 }
             }
